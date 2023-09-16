db09ac174964bc467fcb498e59efafa3bf2f23d1
Maxwell Countryman Skewes <maxskewes@gmail.com>
2023-09-16 14:19:21 -0700

    Image folder added with content pics

---
+++ src/assets/recipe_imgs.js
@@ -0,0 +1,39 @@
+export const recipeImgs = [
+  {
+    title: 'bbq_sando.png',
+    img_src: '/images/recipe_imgs/bbq_sando.png',
+  },
+  {
+    title: 'bbq_sando_above.png',
+    img_src: '/images/recipe_imgs/bbq_sando_above.png',
+  },
+  {
+    title: 'veggies.png',
+    img_src: '/images/recipe_imgs/veggies.png',
+  },
+  {
+    title: 'veggies_rice_bowl.png',
+    img_src: '/images/recipe_imgs/veggies_rice_bowl.png',
+  },
+  //   {
+  //     title: 'veggies_rice_bowl_above.png',
+  //     img_src: '/images/recipe_imgs/veggies_rice_bowl_above.png',
+  //   },
+  {
+    title: 'salad.png',
+    img_src: '/images/recipe_imgs/salad.png',
+  },
+  {
+    title: 'salad_above.png',
+    img_src: '/images/recipe_imgs/salad_above.png',
+  },
+
+  {
+    title: 'tacos.png',
+    img_src: '/images/recipe_imgs/tacos.png',
+  },
+  {
+    title: 'tacos_above.png',
+    img_src: '/images/recipe_imgs/tacos_above.png',
+  },
+];
